9a80ff1ad95f330ab0775b80bc90a3a6ab989ef2
Karthik <jkarthik72@gmail.com>
2014-06-06 13:22:33 +0530

    splash screen

--- www/js/index.js
+++ www/js/index.js
@@ -33,8 +33,8 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-		navigator.splashscreen.show();
 		app.receivedEvent('deviceready');
+		navigator.splashscreen.show();
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
